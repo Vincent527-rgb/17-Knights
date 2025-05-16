07b9aba9cddacd5da7396ac00bdbf90a560b1331
Vincent527-rgb <vincentcss16@gmail.com>
2025-05-16 23:29:07 +0200

    reset

--- scripts/main.js
+++ scripts/main.js
@@ -35,45 +35,51 @@ function displayKnights(name, strength, magic, mana, hp, potions) {
         alert("Veuillez remplir les champs")
     } else {
 
-        // Création container pour afficher mes card personnage
-        const divListCharacter      = document.createElement("div");
-        divListCharacter.className  = "character-card";
-        tagListPlayer.append(divListCharacter);
+        // Vider ma div container
+        tagListPlayer.innerHTML = "";
 
-        // Création des mes div (infos personnage)
-        const divCardName       = document.createElement("div");
-        const divCardStrength   = document.createElement("div");
-        const divCardMagic      = document.createElement("div");
-        const divCardMana       = document.createElement("div");
-        const divCardHp         = document.createElement("div");
-        const divCardPotions    = document.createElement("div");
-        const divCardHpBar      = document.createElement("div");
+        knights.forEach(knight => {
+            // Création container pour afficher mes card personnage
+            const divListCharacter      = document.createElement("div");
+            divListCharacter.className  = "character-card";
+            tagListPlayer.append(divListCharacter);
 
-        // Création d'une classe pour CSS
-        divCardName.className       = "character-card__name";
-        divCardStrength.className   = "character-card__strength";
-        divCardMagic.className      = "character-card__magic";
-        divCardMana.className       = "character-card__mana";
-        divCardHp.className         = "character-card__hp";
-        divCardPotions.className    = "character-card__potions";
-        divCardHpBar.className      = "character-card__hp-bar";
+            // Création des mes div (infos personnage)
+            const divCardName       = document.createElement("div");
+            const divCardStrength   = document.createElement("div");
+            const divCardMagic      = document.createElement("div");
+            const divCardMana       = document.createElement("div");
+            const divCardHp         = document.createElement("div");
+            const divCardPotions    = document.createElement("div");
+            const divCardHpBar      = document.createElement("div");
 
-        // Insertion du contenu 
-        divCardName.textContent     = `${name}`
-        divCardStrength.textContent = `💪 Force : ${strength}`
-        divCardMagic.textContent    = `✨ Magie : ${magic}`
-        divCardMana.textContent     = `🔮 Mana : ${mana}`
-        divCardHp.textContent       = `❤️ Vie : ${hp}`
-        divCardPotions.textContent  = `🧉 Potions : ${potions}`
+            // Création d'une classe pour CSS
+            divCardName.className       = "character-card__name";
+            divCardStrength.className   = "character-card__strength";
+            divCardMagic.className      = "character-card__magic";
+            divCardMana.className       = "character-card__mana";
+            divCardHp.className         = "character-card__hp";
+            divCardPotions.className    = "character-card__potions";
+            divCardHpBar.className      = "character-card__hp-bar";
 
-        // Mise des div infos dans le container (dans l'ordre d'affichage)
-        divListCharacter.append(divCardName);
-        divListCharacter.append(divCardStrength);
-        divListCharacter.append(divCardMagic);
-        divListCharacter.append(divCardMana);
-        divListCharacter.append(divCardHp);
-        divListCharacter.append(divCardPotions);
-        divListCharacter.append(divCardHpBar);
+            // Insertion du contenu 
+            divCardName.textContent     = `${knight.name}`
+            divCardStrength.textContent = `💪 Force : ${knight.strength}`
+            divCardMagic.textContent    = `✨ Magie : ${knight.magic}`
+            divCardMana.textContent     = `🔮 Mana : ${knight.mana}`
+            divCardHp.textContent       = `❤️ Vie : ${knight.hp}`
+            divCardPotions.textContent  = `🧉 Potions : ${knight.potions}`
+
+            // Mise des div infos dans le container (dans l'ordre d'affichage)
+            divListCharacter.append(divCardName);
+            divListCharacter.append(divCardStrength);
+            divListCharacter.append(divCardMagic);
+            divListCharacter.append(divCardMana);
+            divListCharacter.append(divCardHp);
+            divListCharacter.append(divCardPotions);
+            divListCharacter.append(divCardHpBar);
+        })
+        
     }   
 }
 
